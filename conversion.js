7dbbe1c99d8f5ec9fcd00e7970e12ab5e00a7143
Bindu <bindus@thoughtworks.com>
2016-11-23 11:19:06 +0530

    Refactoring

--- conversion.js
+++ conversion.js
@@ -1,22 +1,8 @@
-var converter = require('number-to-words');
 var Parser = require('jison').Parser;
-var Node = require(process.cwd() + "/Node.js");
-var Tree = require(process.cwd() + "/ParseTree.js");
 var fs = require('fs');
 
 var grammar = fs.readFileSync('expression.jison', 'utf8');
 var parser = new Parser(grammar);
-
-var operatorRep = {'+':'plus','*':'times'};
-var operators = Object.keys(operatorRep);
-
-var isAnOperator = function (symbol) {
-    return operators.indexOf(symbol) > -1;
-}
-
-var getWordRep = function (num) {
-    return converter.toWords(num);
-}
 
 var replaceSymbolsFrom = function (str) {
   return str.replace(/\,/g,' ').replace(/\[/,'').replace(/\]/,'').replace(/\"/g,'');
@@ -27,28 +13,14 @@ var restructure = function (list) {
     return replaceSymbolsFrom(JSONString);
 }
 
-var getAsValues = function (symbol) {
-    return isAnOperator(symbol) ? symbol : symbol.value;
+var addParentheses = function (expression) {
+    return expression.evaluate();
 }
 
-var convertIntoWords = function (symbol) {
-  return isAnOperator(symbol) ? operatorRep[symbol] : getWordRep(symbol.value);
+var convertIntoWords = function (expression) {
+    return expression.convertIntoWords();
 }
 
-var convert = function (expression, list, convertForm) {
-    list.push('(');
-    for (var key in expression) {
-      var symbol = expression[key];
-      if(symbol instanceof Tree)
-        convert(symbol, list, convertForm);
-      else {
-        var result = (convertForm == 'words') ? convertIntoWords(symbol) : getAsValues(symbol);
-        list.push(result);
-      }
-    }
-    list.push(')');
-    return restructure(list);
-}
-
-var result = parser.parse("1*2+3");
-console.log(convert(result, [], 'words'));
+var result = parser.parse("5+4*3");
+console.log('tree--> ',result);
+console.log(convertIntoWords(result));
